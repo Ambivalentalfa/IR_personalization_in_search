f7f14cc5a1b0f27621061280e31493e62f71c32c
Diego Bartoli Geijo <114245969+dieGeijo@users.noreply.github.com>
2022-11-21 17:51:51 +0100

    Update create_profiles.py
    Added Alma's code to read the queries from txt file. Setted the decided session structure (number of queries, links, ...) .Everything works well. Still to do the part for the enviromental queries.

--- create_profiles.py
+++ create_profiles.py
@@ -8,6 +8,20 @@ import random
 import json
 from selenium.webdriver.chrome.service import Service
 
+#this method takes in input a file and returns a dictionary with the following structure
+#q_dict={category1: [list of queries for category1], ... }
+
+def query_todict(file):
+    q_dict={}
+    with open(file) as f:
+        for line in f:
+            if line.startswith(">"):
+                header=line.strip(">").strip("\n")
+                q_dict[header]=[]
+            else:
+                q_dict[header].append(line.strip("\n"))
+    return q_dict
+
 def set_driver():
     ser = Service("C:\Drivers\chromedriver.exe")
     chrome_options = webdriver.ChromeOptions()
@@ -41,36 +55,36 @@ def search_news(driver, query):
     q.send_keys(query)
     time.sleep(2)
     q.send_keys(Keys.ENTER)
-    time.sleep(3)
+    time.sleep(5)
 
 def url_navigation(driver, results, query, session_id, user_id):
-    for i in range(3):#explore 3 of the results
+    links_number = random.randint(2,3)
+    for i in range(links_number):#explore 2 or 3 of the results
         # get all the titles, have to do it inside the for otherwise doesn't work
         allurls = driver.find_elements(By.CLASS_NAME, 'title')
 
         #click on random one and save it in the dictionary
         url = allurls[random.randint(1, len(allurls) - 1)].get_attribute('href')
         driver.get(url)
-        time.sleep(5)
+        time_for_link = random.randint(10, 15)
+        time.sleep(time_for_link)#stay from 10 to 15 seconds on the url
         results[user_id][session_id][query].append(url)
 
         driver.back()
+        time.sleep(3)
 
-def browsing_session(results, user_id, session_id, queries):
-    queries_copy = queries #use a copy to be able to remove the visited queries and not visit the same query more times in the same session
+def user_session(results, user_id, session_id, queries):
     driver = login(set_driver(), results, user_id)
     #at this point the driver is in the bing news page, logged
-
     results[user_id].update({session_id: {}})#add the session to the user in the results dictionary
-    for i in range(5):
-        query = queries_copy[random.randint(0, len(queries_copy) - 1)]
+    #for each category
+    for category in queries.keys():
+        #randomly select one query
+        query = queries[category][random.randint(0, len(queries[category]) - 1)]
         results[user_id][session_id].update({query: []})#add the query to the session in the results dicitonary
         search_news(driver, query)#search the query
-
         url_navigation(driver, results, query, session_id, user_id)#explore the results and save the vistited urls
         time.sleep(3)
-        queries_copy.remove(query)
-    driver.close()
 
 def create_results():
     #create the results dictionary
@@ -81,66 +95,41 @@ def create_results():
     results = {}
     for id in user_ids[:1]:
         id_dict = {'username': usernames[id - 1], 'password': passwords[id - 1]}
-        results.update({str(id): id_dict})
+        results.update({id: id_dict})
 
     return results
 
+def browsing_session(results_rightwing, results_leftwing, q_rw, q_lw, session_id):
+    for id in list(range(1,2)):
+        #user_rw = results_rightwing[id]['username']
+        #user_lw = results_leftwing[id]['username']
+        # user of rigth wing
+        user_session(results_rightwing, id, str(session_id), q_rw)
+        time.sleep(60)#how much??
+        # user of left wing
+        user_session(results_leftwing, id, str(session_id), q_lw)
+
 def main():
-    #TO RUN JUST IN THE FIRST SESSION, to create the dictionary
+    #RESULTS, TO RUN JUST IN THE FIRST SESSION, to create the dictionary
     results_rightwing = create_results()
+    results_leftwing = create_results()
     #we save the dicitionary in a json and we reuploaded it for the next sessions
     #with open('results_rw.json') as json_file:
         #results_rightwing = json.load(json_file)
 
-    #lists of queries
-    q_rw_politics = ['PP espana', 'VOX espana', 'noticias estados unidos', 'migracion ilegal']
+    #QUERIES
+    filelw = "Qleft.txt"
+    q_lw = query_todict(filelw)
+    filerw = "Qright.txt"
+    q_rw = query_todict(filerw)
 
-    #run in the decided browsing session schedule, with the correspondant session_id
-    for user in list(range(1, 2)):
-        browsing_session(results_rightwing, str(user), str(2), q_rw_politics)
+    #BROWSING SESSION HAVE TO CHANGE JUST THE SESSION_ID
+    browsing_session(results_rightwing, results_leftwing, q_rw, q_lw, 1)
 
     #save the session results in the results dictionary
-    with open("results_rw.json", "w") as outfile:
+    with open("results_rw.json", "a") as outfile:
          json.dump(results_rightwing, outfile)
+    with open("results_lw.json", "a") as outfile:
+         json.dump(results_leftwing, outfile)
 
 main()
-
-
-#general plan
-#users
-#30, females
-#VPN, location Madrid
-#20 users rw and 20 lw
-#browsing session
-#1 right, 1 left
-#4 days, two browsing sessions per day
-#save the number of queries and links visited
-#10 minutes between each query ?
-#number of queries: 1 query for category
-#number of links: random number between 2 and 3 for each query
-#between 10 and 15 seconds in each link
-
-#enviromental
-#do it once before starting and once at the end
-#5 enviromental queries for all the users
-#save all the results and the rank
-
-#have to decide how to schedule the browsing sessions, maybe one in the morning and one in the afternoon for every user
-#for x days, also have to decide the structure of the sessions, how many queries from each list, how many time for each query, ...
-#save browsing history in a dictionary like this
-#results{
-#user id:dict{
-    #username: username
-    #password: password
-    #session_id_value:{
-        #query_value: [links visited for the query] }
-# } ...
-# }
-#one dictionary for right wing users one for left wing users
-#things to do:
-# - create all the profiles, to have usernames and passwords, with the decided age and location
-# - finish the queries
-# - pass these to this code
-# - write a new method to save the results of the enviromental queries results
-#   for that queries I think we should save all the results and the rank, this code is just to 'train' the profiles
-#   and save the browsing data for completness
